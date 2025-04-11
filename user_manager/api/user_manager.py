fbefa3c80756562fd5da959a8ebb2387821d10df
kiarie <kagglekiarie@gmail.com>
2025-04-11 15:58:18 +0300

    initial setup

--- user_manager/api/user_manager.py
+++ user_manager/api/user_manager.py
@@ -1,6 +1,7 @@
 from django.urls import re_path
 from user_manager.models import CustomUser, UserWallet
 from django.db.models import Q
+from django.http import JsonResponse
 
 
 class UserManagement(object):
@@ -22,40 +23,40 @@ class UserManagement(object):
         try:
             full_name = kwargs.get("full_name")
             if not full_name:
-                return {"status": "failed", "message": "Missing full name"}
+                return JsonResponse({"status": "failed", "message": "Missing full name"})
             email = kwargs.get("email")
             if not email:
-                return {"status": "failed", "message": "Missing email"}
+                return JsonResponse({"status": "failed", "message": "Missing email"})
             phone_number = kwargs.get("phone_number")
             if not phone_number:
-                return {"status": "failed", "message": "Missing phone number"}
+                return JsonResponse({"status": "failed", "message": "Missing phone number"})
             password = kwargs.get("password")
             if not password:
-                return {"status": "failed", "message": "Missing password"}
+                return JsonResponse({"status": "failed", "message": "Missing password"})
             # Create user first
             user = CustomUser.objects.get(phone_number=phone_number)
             if user:
-                return {"status": "failed", "message": "User already exists"}
+                return JsonResponse({"status": "failed", "message": "User already exists"})
             user = CustomUser.objects.create(
                 full_name=full_name, email=email, phone_number=phone_number, password=password
             )
             if not user:
-                return {"status": "failed", "message": "Failed creating user"}
+                return JsonResponse({"status": "failed", "message": "Failed creating user"})
             # Set user password
             user.set_password(password)
             # Create user wallet
             user_wallet = UserWallet.objects.get(user=user)
             if user_wallet:
-                return {"status": "failed", "message": "User wallet already exists"}
+                return JsonResponse({"status": "failed", "message": "User wallet already exists"})
             user_wallet = UserWallet.objects.create(
                 user=user, wallet_balance=0.0, wallet_currency="KES", account_number=phone_number
             )
             if not user_wallet:
-                return {"status": "failed", "message": "Failed creating user wallet"}
-            return {"code": "100.000.000", "status": "success", "context": "User created successfully"}
+                return JsonResponse({"status": "failed", "message": "Failed creating user wallet"})
+            return JsonResponse({"code": "100.000.000", "status": "success", "context": "User created successfully"})
         except Exception as e:
             pass  # lgr.exception('%s Service create exception: %s' % (self.manager.model.__name__, e))
-        return {"code": "500.001.012", "status": "failed", "context": "Failed creating user"}
+        return JsonResponse({"code": "500.001.012", "status": "failed", "context": "Failed creating user"})
 
     def authenticate_user(self, **kwargs):
         """
