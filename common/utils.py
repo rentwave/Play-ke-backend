236196b6df6f85b9638221ca698acf365eb228e8
kiarie <kagglekiarie@gmail.com>
2025-04-14 12:20:35 +0300

    user manager models

---
+++ common/utils.py
@@ -0,0 +1,35 @@
+import json
+from django.http import QueryDict
+
+
+def get_request_data(request):
+	"""
+	Retrieves the request data irrespective of the method and type it was send.
+	@param request: The Django HttpRequest.
+	@type request: WSGIRequest
+	@return: The data from the request as a dict
+	@rtype: QueryDict
+	"""
+	try:
+		data = None
+		if request is not None:
+			request_meta = getattr(request, 'META', {})
+			request_method = getattr(request, 'method', None)
+			if request_meta.get('CONTENT_TYPE', '') == 'application/json':
+				data = json.loads(request.body)
+			elif request_method == 'GET':
+				data = request.GET.copy()
+				data = data.dict()
+			elif request_method == 'POST':
+				data = request.POST.copy()
+				data = data.dict()
+			if not data:
+				request_body = getattr(request, 'body', None)
+				if request_body:
+					data = json.loads(request_body)
+				else:
+					data = QueryDict()
+			return data
+	except Exception as e:
+		print('get_request_data Exception: %s', e)
+	return QueryDict()
