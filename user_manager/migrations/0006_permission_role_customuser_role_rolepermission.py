442c18b8243d84513e12a8e89a97e1edd9459a4f
kiarie <kagglekiarie@gmail.com>
2025-04-16 10:01:50 +0300

    migration fix

--- user_manager/migrations/0006_permission_role_customuser_role_rolepermission.py
+++ user_manager/migrations/0006_permission_role_customuser_role_rolepermission.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0004_state_name'),
+        # ('base', '0004_state_name'),
         ('user_manager', '0005_remove_customuser_name_remove_identity_name_and_more'),
     ]
 
