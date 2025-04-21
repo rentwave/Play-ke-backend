85d04eb0abf48714fbfa77356b18eb1d7892feeb
Osiru <marikwamari023@gmail.com>
2025-04-21 19:15:17 +0400

    log auth

--- common/decorators.py
+++ common/decorators.py
@@ -6,18 +6,25 @@ from base.models import SystemClient
 def auth_required(view_func):
     @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
-        auth_header = request.META.get('HTTP_AUTHORIZATION', '')
-        print("Auth Header : -",auth_header)
+        print(request.headers)
+        auth_header = request.headers.get('Authorization', '')
+        print("Auth Header:", auth_header)
+
         if not auth_header.startswith("Bearer "):
             return JsonResponse({'error': 'Missing or invalid Authorization header. Use: /auth/token/'}, status=401)
+
         token = auth_header[len("Bearer "):]
+
         try:
             client = SystemClient.objects.get(access_token=token, is_active=True)
         except SystemClient.DoesNotExist:
             return JsonResponse({'error': 'Invalid or expired token. Use: /auth/token/'}, status=401)
-        if client.token_expiry < timezone.now():
+
+        if client.token_expiry and client.token_expiry < timezone.now():
             client.is_active = False
             client.save()
             return JsonResponse({'error': 'Token expired. Use: /auth/token/'}, status=401)
+
         return view_func(request, *args, **kwargs)
+
     return _wrapped_view
